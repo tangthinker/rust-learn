7e6a1a921aaf25c765f6fa2967781eb620782a37
tangyubin <tangyubin@tal.com>
2025-04-20 17:52:45 +0800

    feat: owership

--- _06_owership/main.rs
+++ _06_owership/main.rs
@@ -1,2 +1,19 @@
 // Owership
 // Rust使用Owership系统的一系列编译时规则来管理内存
+fn main(){
+    {
+        let s1 = String::from("hello");
+        let s2 = s1;
+
+        // println!("{s1}, world!");        // Error s1 was moved to s2, s1 is not valid any more
+        println!("{s2}, world!");
+    }
+
+    {
+        let s1 = String::from("hello");
+        let s2 = s1.clone();
+
+        println!("{s1}, world!");
+        println!("{s2}, world!");
+    }
+}
